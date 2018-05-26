d1c397654f63dd89bb4801189cb1992d0e734a8f
Tammo0987 <tammo.steffens@googlemail.com>
2018-05-26 22:10:34 +0200

    Added wrapper key validation
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Wrapper/src/main/java/de/tammo/cloud/wrapper/setup/WrapperSetup.java
+++ Open-Wrapper/src/main/java/de/tammo/cloud/wrapper/setup/WrapperSetup.java
@@ -8,7 +8,7 @@ import de.tammo.cloud.core.logging.Logger;
 import de.tammo.cloud.core.setup.Setup;
 import de.tammo.cloud.core.setup.requests.StringRequest;
 import de.tammo.cloud.wrapper.Wrapper;
-import de.tammo.cloud.wrapper.network.packets.WrapperKeyOutPacket;
+import de.tammo.cloud.wrapper.network.packets.out.WrapperKeyOutPacket;
 import jline.console.ConsoleReader;
 
 import java.io.IOException;
@@ -17,9 +17,11 @@ public class WrapperSetup implements Setup {
 
 	public void setup(final Logger logger, final ConsoleReader reader) throws IOException {
 		if (Wrapper.getWrapper().getConfiguration().getKey().isEmpty()) {
-			new StringRequest().request(logger, "Type in the key, you received from the Open-Master", reader, key ->
-					Wrapper.getWrapper().setKey(key));
+			new StringRequest().request(logger, "Type in the key, you received from the Open-Master", reader, key -> Wrapper.getWrapper().setKey(key));
 			Wrapper.getWrapper().getNetworkHandler().sendPacketToMaster(new WrapperKeyOutPacket(Wrapper.getWrapper().getKey()));
+		} else {
+			Wrapper.getWrapper().getNetworkHandler().sendPacketToMaster(new WrapperKeyOutPacket(Wrapper.getWrapper()
+					.getConfiguration().getKey()));
 		}
 	}
 
