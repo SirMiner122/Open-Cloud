5641448fe6292f0b0859d31854d6e24f4f785041
Tammo0987 <tammo.steffens@googlemail.com>
2018-06-02 15:35:00 +0200

    Removed copyDir method
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Core/src/main/java/de/tammo/cloud/core/file/FileUtils.java
+++ Open-Core/src/main/java/de/tammo/cloud/core/file/FileUtils.java
@@ -16,20 +16,6 @@ import java.util.Objects;
 
 public class FileUtils {
 
-	public static void copyDir(final File from, final File to) throws IOException {
-		if (!Files.notExists(to.toPath())) {
-			Files.createDirectories(to.toPath());
-		}
-
-		for (final File file : Objects.requireNonNull(from.listFiles())) {
-			if (file.isDirectory()) {
-				copyDir(file, new File(to.getAbsolutePath() + "//" + file.getName()));
-			} else {
-				Files.copy(file.toPath(), new File(to.getAbsolutePath() + "//" + file.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
-			}
-		}
-	}
-
 	public static void deleteDir(final File dir) throws IOException {
 		if (!Files.exists(dir.toPath())) throw new FileNotFoundException();
 
