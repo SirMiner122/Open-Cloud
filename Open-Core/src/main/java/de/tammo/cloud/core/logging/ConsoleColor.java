ac43f50a35bfc64a9a4cf7c297a9fb81943a2183
Jake <x7airworker@gmail.com>
2018-08-06 22:35:23 +0200

    Added enum for Colored Console Output

---
+++ Open-Core/src/main/java/de/tammo/cloud/core/logging/ConsoleColor.java
@@ -0,0 +1,26 @@
+package de.tammo.cloud.core.logging;
+
+public enum ConsoleColor
+{
+    RESET  ("\u001B[0m"),
+    BLACK  ("\u001B[30m"),
+    RED    ("\u001B[31m"),
+    GREEN  ("\u001B[32m"),
+    YELLOW ("\u001B[33m"),
+    BLUE   ("\u001B[34m"),
+    PURPLE ("\u001B[35m"),
+    CYAN   ("\u001B[36m"),
+    WHITE  ("\u001B[37m");
+
+    private final String colorString;
+
+    ConsoleColor(String colorString)
+    {
+        this.colorString = colorString;
+    }
+
+    public String getColorString()
+    {
+        return colorString;
+    }
+}
