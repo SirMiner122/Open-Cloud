bc62d9c9a19c7806c1ed59355c896006a1b87f9e
Tammo0987 <tammo.steffens@googlemail.com>
2018-08-12 16:12:02 +0200

    Docs written for the ThreadBuilder
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Core/src/main/java/de/tammo/cloud/core/threading/ThreadBuilder.java
+++ Open-Core/src/main/java/de/tammo/cloud/core/threading/ThreadBuilder.java
@@ -6,23 +6,56 @@
 
 package de.tammo.cloud.core.threading;
 
+/**
+ * Builder util to start and build {@link Thread}s easily
+ *
+ * @author Tammo
+ * @version 1.0
+ * @since 1.0
+ */
 public class ThreadBuilder {
 
+	/**
+	 * {@link Thread} object to build and configure
+	 */
 	private final Thread thread;
 
+	/**
+	 * Create a new {@link ThreadBuilder}
+	 *
+	 * @param name Name of the {@link Thread}
+	 * @param runnable {@link Runnable} to run in the new {@link Thread}
+	 *
+	 * @since 1.0
+	 */
 	public ThreadBuilder(final String name, final Runnable runnable) {
 		this.thread = new Thread(runnable, name);
 	}
 
+	/**
+	 * @return The current instance after configure the {@link Thread} as daemon
+	 *
+	 * @since 1.0
+	 */
 	public final ThreadBuilder setDaemon() {
 		this.thread.setDaemon(true);
 		return this;
 	}
 
+	/**
+	 * Start {@link Thread} without a return value
+	 *
+	 * @since 1.0
+	 */
 	public void startThread() {
 		this.thread.start();
 	}
 
+	/**
+	 * @return The builded {@link Thread} and starts the {@link Thread}
+	 *
+	 * @since 1.0
+	 */
 	public final Thread start() {
 		this.thread.start();
 		return this.thread;
