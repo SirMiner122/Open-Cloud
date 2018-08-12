6a00ae6e85c925e7f9940e25ff926197d24d18f9
Tammo0987 <tammo.steffens@googlemail.com>
2018-08-12 16:31:25 +0200

    Refactored the Workload system
    Signed-off-by: Tammo0987 <tammo.steffens@googlemail.com>

--- Open-Core/src/main/java/de/tammo/cloud/core/workload/Workload.java
+++ Open-Core/src/main/java/de/tammo/cloud/core/workload/Workload.java
@@ -8,11 +8,28 @@ package de.tammo.cloud.core.workload;
 
 import lombok.Value;
 
+/**
+ * {@link Workload} object with information about the current average usage of the system
+ *
+ * @author Tammo
+ * @version 1.0
+ * @since 1.0
+ */
 @Value
 public class Workload {
 
+	/**
+	 * Current cpu usage in percent
+	 *
+	 * @since 1.0
+	 */
 	private final int cpu;
 
+	/**
+	 * Current usage of the memory in percent
+	 *
+	 * @since 1.0
+	 */
 	private final int memory;
 
 }
--- Open-Core/src/main/java/de/tammo/cloud/core/workload/WorkloadFactory.java
+++ Open-Core/src/main/java/de/tammo/cloud/core/workload/WorkloadFactory.java
@@ -7,25 +7,56 @@
 package de.tammo.cloud.core.workload;
 
 import com.sun.management.OperatingSystemMXBean;
-
 import java.lang.management.ManagementFactory;
 
+/**
+ * Check the current usage of the system components
+ *
+ * @author Tammo
+ * @version 1.0
+ * @since 1.0
+ */
 public class WorkloadFactory {
 
+	/**
+	 * {@link OperatingSystemMXBean} to check the usages
+	 *
+	 * @since 1.0
+	 */
 	private final OperatingSystemMXBean os = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 
+	/**
+	 * @return A new {@link Workload} object of the current usages
+	 *
+	 * @since 1.0
+	 */
 	public final Workload newWorkload() {
 		return new Workload(this.getCpuUsage(), this.getMemory());
 	}
 
+	/**
+	 * @return The current usage of the cpu in percent
+	 *
+	 * @since 1.0
+	 */
 	private int getCpuUsage() {
 		return (int) (this.os.getSystemCpuLoad() * 100);
 	}
 
+	/**
+	 * @return The currently used size of the memory
+	 *
+	 * @since 1.0
+	 */
 	private long getUsedSystemMemory() {
 		return this.os.getTotalPhysicalMemorySize() - this.os.getFreePhysicalMemorySize();
 	}
 
+	/**
+	 * @return Currently used memory in percent
+	 *
+	 * @since 1.0
+	 */
 	private int getMemory() {
 		return (int) (((double) this.getUsedSystemMemory() / this.os.getTotalPhysicalMemorySize()) * 100);
 	}
